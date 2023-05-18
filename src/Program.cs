82136a7263a4484670c36455f314cd395d99c50b
pettijohn <632844+pettijohn@users.noreply.github.com>
2023-05-18 14:32:15 -0700

    Successfully ping telegram

--- src/Program.cs
+++ src/Program.cs
@@ -2,11 +2,21 @@ using System.Net;
 using System.Reflection;
 using System.Xml;
 using System.Xml.Serialization;
+using Microsoft.VisualBasic;
 
 
 // Build Environment
 var wxStation = Environment.GetEnvironmentVariable("WXSTATION");
 if (string.IsNullOrEmpty(wxStation)) throw new ArgumentNullException("WXSTATION");
+
+var telegramUrl = Environment.GetEnvironmentVariable("TELEGRAM_URL");
+if (String.IsNullOrEmpty(telegramUrl))
+{
+    DotEnv.Load(".env");
+}
+telegramUrl = Environment.GetEnvironmentVariable("TELEGRAM_URL");
+if (String.IsNullOrEmpty(telegramUrl)) throw new ArgumentNullException("TELEGRAM_URL");
+
 var http = new HttpClient();
 var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 http.DefaultRequestHeaders.Add("User-Agent", $"CheckAndNotify/{version}"); // required by NWS
@@ -18,5 +28,33 @@ var rawXml = await http.GetStringAsync(url);
 XmlSerializer serializer = new XmlSerializer(typeof(CurrentObservation));
 using (StringReader reader = new StringReader(rawXml))
 {
-   var test = (CurrentObservation)serializer.Deserialize(reader);
+    var test = (CurrentObservation)serializer.Deserialize(reader)!;
+    var message = test.ObservationTimeRfc822;
+    message = message.Substring(0, message.Length < 4000 ? message.Length : 4000);
+    await http.GetAsync(telegramUrl + "&text=" + System.Net.WebUtility.UrlEncode(message));
+
+    // create real message
+}
+
+public static class DotEnv
+{
+    public static void Load(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            return;
+        }
+
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            var i = line.IndexOf('=');
+            if (i == 0) continue;
+            var key = line.Substring(0, i);
+            var val = line.Substring(i + 1);
+
+            val = val.TrimStart('"').TrimEnd('"');
+
+            Environment.SetEnvironmentVariable(key, val);
+        }
+    }
 }
